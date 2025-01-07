6b798a85419b11b1e591abc5306161e64ccf6234
Gaurab Neupane <gaurabn23@gmail.com>
2025-01-07 21:52:22 +0545

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -4,14 +4,7 @@ import { getDatabase } from 'firebase/database';
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyA1yGqCpXLPXwJqHRbftgmXWWcydJMI2Lg",
-    authDomain: "dots-41c5f.firebaseapp.com",
-    databaseURL: "https://dots-41c5f-default-rtdb.firebaseio.com",
-    projectId: "dots-41c5f",
-    storageBucket: "dots-41c5f.appspot.com",
-    messagingSenderId: "1098098293300",
-    appId: "1:1098098293300:web:f508d984442b3800476861",
-    measurementId: "G-GH2Y0YWH2X",
+    //config details
 };
 
 
